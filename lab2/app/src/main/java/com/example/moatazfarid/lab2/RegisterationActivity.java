1642d8043098d2921aba019fadbf5eac08ba6e80
Moataz Farid <moatazmfarid@gmail.com>
2017-03-13 22:39:33 +0200

    lab2 finished

---
+++ lab2/app/src/main/java/com/example/moatazfarid/lab2/RegisterationActivity.java
@@ -0,0 +1,65 @@
+package com.example.moatazfarid.lab2;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class RegisterationActivity extends Activity {
+
+    //define vars
+    TextView txtView_login ;
+    EditText txt_name;
+    EditText txt_email;
+    EditText txt_pass;
+    EditText txt_passConfirm;
+    Button btn_register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registeration);
+
+        // mapping layout
+        txtView_login = (TextView) findViewById(R.id.txtView_registration_login);
+        txt_name = (EditText) findViewById(R.id.txt_name);
+        txt_email = (EditText) findViewById(R.id.txt_email);
+        txt_pass = (EditText) findViewById(R.id.txt_pass);
+        txt_passConfirm = (EditText) findViewById(R.id.txt_passConfirm);
+        btn_register = (Button) findViewById(R.id.btn_register);
+
+        // listeners
+        txtView_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterationActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = txt_name.getText().toString();
+                String email = txt_email.getText().toString();
+                String pass = txt_pass.getText().toString();
+                String passconfirm = txt_passConfirm.getText().toString();
+
+                // view toast
+                Toast.makeText(RegisterationActivity.this,"Thanks "+name+" , You have Registered !!",Toast.LENGTH_LONG).show();
+
+                // redirect to MainActivity
+                Intent intent = new Intent(RegisterationActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+}
